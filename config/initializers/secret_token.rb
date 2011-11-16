78c56caa0bbbb3addfee316181108aa11c91d305
Braden <braden.douglass@gmail.com>
2011-11-16 11:26:45 -0500

    origin commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstOrders::Application.config.secret_token = 'ee31f71caadae50ad807eafdb3b14b352829de341b8bff506b5e293a3e6cd8c36313951f40f8e9d9e36172dc0ecf6c63df14a09acb5364e584a314313bb62e19'
