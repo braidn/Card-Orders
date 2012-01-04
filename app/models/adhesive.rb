cb31db4ad8dd8172cd53c9e838dc2d761bc0e570
Braden <braden.douglass@gmail.com>
2012-01-04 14:12:31 -0500

    The slog through validations begins

--- app/models/adhesive.rb
+++ app/models/adhesive.rb
@@ -1,2 +1,9 @@
 class Adhesive < ActiveRecord::Base
+  #t.string :adhesivedescription
+  #t.integer :costperlb
+  #t.integer :msiperlb
+  #t.string :partnumber
+  #t.string :primaryuse
+
+  validates :adhesivedescription, :costperlb, :msiperlb, :partnumber, :primaryuse, :presence => true
 end
