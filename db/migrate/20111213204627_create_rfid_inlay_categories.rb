05914a21679215f2a2e7430c00b340d51cce2e51
Braden <braden.douglass@gmail.com>
2012-02-02 14:44:50 -0500

    string was incorrect on the inlaytype and needed to be grammatically fixed

--- db/migrate/20111213204627_create_rfid_inlay_categories.rb
+++ db/migrate/20111213204627_create_rfid_inlay_categories.rb
@@ -1,7 +1,7 @@
 class CreateRfidInlayCategories < ActiveRecord::Migration
   def change
     create_table :rfid_inlay_categories do |t|
-      t.string--spec :inlayType
+      t.string :inlayType
 
       t.timestamps
     end
