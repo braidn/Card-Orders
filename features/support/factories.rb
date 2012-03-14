365aad162902b574ccdc022786e31b8ff981d64d
Braden <braden.douglass@gmail.com>
2012-03-14 12:04:05 -0400

    Factory environment help not needed with factory_girl_rails. The file will be kept around, all documentation on the need/not need is at the top

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -1,3 +1,5 @@
-Dir[Rails.root + "spec/factories/*.rb"].each do |file|
-  include file
-end
+#It is a good possibility that this file can be deleted due factor_girl_rails
+#Currently if it is included it gives a duplicate error
+#Dir[Rails.root + "spec/factories/*.rb"].each do |file|
+  #require file
+#end
