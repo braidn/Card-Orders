85cca8588035b2f0f63f8d460ef192ccf36fa9d3
Braden <braden.douglass@gmail.com>
2012-06-13 16:04:30 -0400

    billings feature finished and all passing. Looks pretty much the same as the rest of the tables due to its CRUDability

---
+++ features/step_definitions/billing_steps.rb
@@ -0,0 +1,36 @@
+Given /^there is a billing with addresstype "(.*?)"$/ do |address|
+  @billing = Fabricate(:billing, :addressType => address)
+end
+
+Given /^I am at the list of billings$/ do
+  visit(billings_path)
+end
+
+When /^I fill in the billing form correctly$/ do
+  @cbilled = Fabricate(:billing)
+  fill_in("Address Type", :with => @cbilled.addressType)
+  fill_in("City", :with => @cbilled.city)
+  fill_in("Country", :with => @cbilled.country)
+  fill_in("Postal Code", :with => @cbilled.postalCode)
+  fill_in("State", :with => @cbilled.state)
+  fill_in("Street1", :with => @cbilled.street1)
+  fill_in("Street1a", :with => @cbilled.street1a)
+  fill_in("Street2", :with => @cbilled.street2)
+  fill_in("Street2a", :with => @cbilled.street2a)
+end
+
+When /^I fill in the billing form incorrectly$/ do
+  fill_in("Address Type", :with => "")
+  fill_in("City", :with => "")
+  fill_in("State", :with => "")
+  fill_in("Country", :with => "")
+  fill_in("Postal Code", :with => "Byte me")
+end
+
+Then /^I am presented with the new billing$/ do
+  page.has_content?(@cbilled.street1)
+end
+
+Then /^I should see specific details about the selected billing$/ do
+  page.has_content?(@billing.addressType)
+end
---
+++ spec/fabricators/billings.rb
@@ -0,0 +1,12 @@
+Fabricator( :billing ) do
+  addressType 'string'
+  city 'string'
+  country 'string'
+  customerId 1
+  postalCode 12345
+  state 'string'
+  street1 'string'
+  street1a 'string'
+  street2 'string'
+  street2a 'string'
+end
