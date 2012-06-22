38457da77c5923e97387108ee561d37070c2848a
Braden <braden.douglass@gmail.com>
2012-06-22 10:22:34 -0400

    removed an unneeded and none working migration that changed core name

--- db/migrate/20120621183108_change_cores_type_column_to_coretype.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCoresTypeColumnToCoretype < ActiveRecord::Migration
-  def up
-    change_column :cores, :type, :coretype
-  end
-
-  def down
-    change_column :cores, :coretype, :type
-  end
-end
